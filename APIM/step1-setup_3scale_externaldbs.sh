69b13d9188d1e276f93e5a6b6fdca4fc93ae64d9
Jean Nyilimbibi <jean.nyilimbibi@gmail.com>
2022-07-11 17:24:38 +0200

    Upgraded images tag: Redis to 6-el8 and Postgresql to 13-el8

--- APIM/step1-setup_3scale_externaldbs.sh
+++ APIM/step1-setup_3scale_externaldbs.sh
@@ -4,7 +4,7 @@
 ## Namespace for the self-managed PostgreSQL DB and Redis
 API_MANAGER_DB_NS="3scale-lab-ossm-databases"
 
-# Create the`3scale-lab-ossm-databases` _OpenShift namespace_
+# Create the`3scale-databases` _OpenShift namespace_
 oc new-project $API_MANAGER_DB_NS \
   --display-name="3scale API Manager Databases (self-managed)" \
   --description=$API_MANAGER_DB_NS
@@ -17,10 +17,7 @@ oc new-app postgresql-persistent --name="threescale-system-postgresql" \
 -p POSTGRESQL_PASSWORD="P!ssw0rd" \
 -p POSTGRESQL_DATABASE="threescaledb" \
 -p VOLUME_CAPACITY="10Gi" \
--p POSTGRESQL_VERSION="12-el8"
-
-# Import the redis 6 image
-oc -n openshift import-image redis:6 --from=registry.redhat.io/rhel8/redis-6 --confirm
+-p POSTGRESQL_VERSION="13-el8"
 
 # Spin-up a persistent Redis instance for the 3scale system
 oc new-app redis-persistent --name="threescale-system-redis" \
@@ -28,7 +25,7 @@ oc new-app redis-persistent --name="threescale-system-redis" \
 -p DATABASE_SERVICE_NAME="threescale-system-redis" \
 -p REDIS_PASSWORD="P!ssw0rd" \
 -p VOLUME_CAPACITY="10Gi" \
--p REDIS_VERSION="6"
+-p REDIS_VERSION="6-el8"
 
 # Spin-up a persistent Redis instance for the 3scale backend
 oc new-app redis-persistent --name="threescale-backend-redis" \
@@ -36,6 +33,6 @@ oc new-app redis-persistent --name="threescale-backend-redis" \
 -p DATABASE_SERVICE_NAME="threescale-backend-redis" \
 -p REDIS_PASSWORD="P!ssw0rd" \
 -p VOLUME_CAPACITY="10Gi" \
--p REDIS_VERSION="6"
+-p REDIS_VERSION="6-el8"
 
 watch oc get po
